0599b550ce567a97f869baa7d5153bb255653720
loopy-lim <loopy@jnu.ac.kr>
2025-05-24 16:59:32 +0900

    feat: add fonts

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router";
 import MapTest from "./components/Maptest";
+import MainPage from "./pages/MainPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <></>,
+  },
+  {
+    path: "/home",
+    element: <MainPage />,
   },
   {
     path: "/test_map",
