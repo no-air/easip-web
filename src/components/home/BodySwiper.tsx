6a8d0e838d5a1a016caf892c539994825cc16b7b
loopy-lim <loopy@jnu.ac.kr>
2025-05-28 14:21:57 +0900

    feat: update design
    - add cn
    - add tailwind marge

--- src/components/home/BodySwiper.tsx
+++ src/components/home/BodySwiper.tsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import type { Swiper as SwiperType } from "swiper";
 import type { GetPostHomeResponse } from "../../apis/dtos/posts";
 import { formatDate } from "../../util/date";
+import { cn } from "../../util/lib";
 
 interface BodySwiperProps {
   setBodySwiper: (swiper: SwiperType) => void;
@@ -55,22 +56,6 @@ const BodySwiper = ({ setBodySwiper, imageSwiper, page }: BodySwiperProps) => {
                 </div>
               </div>
               <div>
-                <span>지원조건</span>
-                <div className="flex gap-2 flex-wrap">
-                  {houseSummaryResponses[0].applicationConditionDtos.map(
-                    ({ content, isApplicable }) =>
-                      isApplicable && (
-                        <div
-                          className="px-2 rounded bg-[#2F82F4] text-white text-xs"
-                          key={content}
-                        >
-                          {content}
-                        </div>
-                      )
-                  )}
-                </div>
-              </div>
-              <div>
                 <span>게시일</span>
                 <div>{formatDate(scheduleDtos[0].start)}</div>
               </div>
@@ -79,10 +64,48 @@ const BodySwiper = ({ setBodySwiper, imageSwiper, page }: BodySwiperProps) => {
                 <div>{formatDate(scheduleDtos[1].end)}</div>
               </div>
             </div>
+            <ConditionsRow houseSummaryResponses={houseSummaryResponses} />
           </SwiperSlide>
         )
       )}
     </Swiper>
+  );
+};
+
+interface ConditionsRowProps {
+  houseSummaryResponses: GetPostHomeResponse["results"][number]["houseSummaryResponses"];
+}
+
+const ConditionsRow = ({ houseSummaryResponses }: ConditionsRowProps) => {
+  return (
+    <div className="flex flex-col mt-5 gap-2">
+      <div className="flex gap-4 items-baseline">
+        <span>지원조건</span>
+        <span className="text-sm before:w-2 before:h-2 before:bg-[#2F82F4] before:inline-block before:mr-1">
+          해당
+        </span>
+        <span className="text-sm before:w-2 before:h-2 before:bg-[#E2E2E2] before:inline-block before:mr-1">
+          비해당
+        </span>
+      </div>
+      <div className="flex gap-2 flex-wrap">
+        {houseSummaryResponses[0].applicationConditionDtos
+          .sort((a, b) => (a.isApplicable ? -1 : b.isApplicable ? 1 : 0))
+          .map(({ content, isApplicable }) => (
+            <div
+              className={cn(
+                "px-4 py-1.5 rounded text-xs",
+                isApplicable
+                  ? "bg-[#2F82F4] text-white"
+                  : "bg-[#E2E2E2] text-[#868686]"
+              )}
+              key={content}
+            >
+              {content}
+            </div>
+          ))}
+      </div>
+    </div>
   );
 };
 
