5e18bcdae7bfaef703367ba997c5a9060bdc78b3
loopy-lim <cotmd6203@naver.com>
2025-06-03 17:47:14 +0900

    feat: update fake token member id

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,7 +1,7 @@
 import { API_URL } from "../constants/api";
 
 export const postFakeToken = () => {
-  const query = new URLSearchParams({ memberId: "testuser" });
+  const query = new URLSearchParams({ memberId: "00000000000000000000000000" });
 
   return fetch(`${API_URL}/v1/auth/fake-token?${query.toString()}`, {
     method: "POST",
